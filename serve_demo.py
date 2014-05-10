60a172cd1cde227ad21585d033a634078d9118e9
Tylar Murray <murray.tylar@gmail.com>
2014-05-10 10:58:36 -0400

    added demo html and basic local host for demo

---
+++ serve_demo.py
@@ -0,0 +1,16 @@
+'''
+Sets up a simple http server so you can view demo.html in your browser at `localhost:8000`
+'''
+
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+print 'open your web browser to http://localhost:8000/demo.html to view,\n\tpress ctrl+c to stop.'
+httpd.serve_forever()
