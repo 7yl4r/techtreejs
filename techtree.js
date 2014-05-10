217529198eee084c3d07a2d329ef1501049dfd0d
Tylar Murray <murray.tylar@gmail.com>
2014-05-10 14:54:47 -0400

    tooltips now much more functional

--- techtree.js
+++ techtree.js
@@ -18,7 +18,7 @@ techtree = {
         var diagonal = d3.svg.diagonal()
             .projection(function(d) { return [d.y, d.x]; });
 
-        var svg = d3.select("body").append("svg")
+        techtree.treeSVG = d3.select("body").append("svg")
             .attr("width", width)
             .attr("height", height)
           .append("g")
@@ -28,21 +28,22 @@ techtree = {
           var nodes = tree.nodes(json),
               links = tree.links(nodes);
 
-          var link = svg.selectAll("path.link")
+          var link = techtree.treeSVG.selectAll("path.link")
               .data(links)
             .enter().append("path")
               .attr("class", "link")
               .attr("d", diagonal);
 
-          var node = svg.selectAll("g.node")
+          var node = techtree.treeSVG.selectAll("g.node")
               .data(nodes)
             .enter().append("g")
               .attr("class", "node")
               .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
-              .attr("onmouseover",function(d){ return "techtree.showTooltip('"+d.text+"',"+d.x+","+d.y+")"; })
+              .attr("onmouseover",function(d){ return "techtree.showTooltip('"+d.name+"','"+d.text+"',"+d.x+","+d.y+")"; })
+              .attr("onmouseout" ,function(d){ return "techtree.unshowTooltip('"+d.name+"')"; })
 
           node.append("circle")
-              .attr("r", 4.5);
+              .attr("r", 7);
 
           node.append("text")
               .attr("dx", function(d) { return d.children ? -8 : 8; })
@@ -53,8 +54,33 @@ techtree = {
 
         d3.select(self.frameElement).style("height", height + "px");
     },
-    showTooltip: function(desc, x, y){
+    
+    showTooltip: function(name, desc, x, y){
         // shows a tooltip for the given node
-        console.log('tooltip:', desc,' @ (',x,',',y,')');
+        X = y;  // yes, x and y are switched here... don't ask me why, they just are.
+        Y = x;
+        W = 500;
+        H = 100;
+        txt_H = H/3;
+        console.log('drawing tooltip for:', name,' @ (',X,',',Y,')');
+        var box = techtree.treeSVG.append('rect')
+                                    .attr('id',name+'_tooltip_box')
+                                    .attr('x',X)
+                                    .attr('y',Y)
+                                    .attr('width',W)
+                                    .attr('height',H)
+                                    .attr('fill','rgba(200,200,200,0.3)');
+              
+        var text = techtree.treeSVG.append('text')
+                                    .attr('id',name+'_tooltip_txt')
+                                    .attr("x",X)
+                                    .attr('y',Y+txt_H)
+                                    .attr('font-size',txt_H)
+                                    .text(desc);
+
+    },
+    unshowTooltip: function(nodename){
+        d3.select('#'+nodename+'_tooltip_box').remove();
+        d3.select('#'+nodename+'_tooltip_txt').remove();
     }
 };
