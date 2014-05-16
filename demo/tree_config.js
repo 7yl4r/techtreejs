3e4b15bbac7d1cb4fa9ca1b47dd1aec4b0bc3c49
Tylar Murray <murray.tylar@gmail.com>
2014-05-16 15:01:27 -0400

    added the critical ,

--- demo/tree_config.js
+++ demo/tree_config.js
@@ -1,7 +1,7 @@
 
 treeConfig = {
     // svgSrc: '/demo/demo_resources/tree.svg',
-    jsonSrc: 'http://7yl4r.github.io/techtreejs/demo/demo_tree.json'
+    jsonSrc: 'http://7yl4r.github.io/techtreejs/demo/demo_tree.json',
     treeWidth: 650,
     treeHeight: 600
 };
