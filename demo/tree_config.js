ce3b07b03c186ce9d259cebf85d78741a45e18f9
Tylar Murray <murray.tylar@gmail.com>
2014-05-16 15:03:47 -0400

    removed unneeded comment

--- demo/tree_config.js
+++ demo/tree_config.js
@@ -4,15 +4,4 @@ treeConfig = {
     jsonSrc: '/demo/demo_tree.json',
     treeWidth: 800,
     treeHeight: 1000
-/*
-    openMenu: function( ) {
-        …
-        this.setMenuStyle( );
-    },
-    closeMenu: function( ) {
-        …
-        this.setMenuStyle( );
-    },
-    setMenuStyle: function( ) { … }
-*/
 };
