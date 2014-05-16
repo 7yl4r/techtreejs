160090eeca460f41f58434f96be6bf4936847635
Tylar Murray <murray.tylar@gmail.com>
2014-05-16 12:35:19 -0400

    changed port to avoid access restrictions

--- serve_demo.py
+++ serve_demo.py
@@ -5,7 +5,7 @@ Sets up a simple http server so you can view demo.html in your browser at `local
 import SimpleHTTPServer
 import SocketServer
 
-PORT = 8000
+PORT = 8080
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
