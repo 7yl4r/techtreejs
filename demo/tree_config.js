f976dfea87d1636d8e4db78ce9bff313f1529c1d
Tylar Murray <murray.tylar@gmail.com>
2014-05-21 23:51:58 -0400

    added pretty little interface for playing with treeConfig

--- demo/tree_config.js
+++ demo/tree_config.js
@@ -4,6 +4,7 @@ treeConfig = {
     jsonSrc: '/demo/demo_tree.json',
     treeWidth: 800,
     treeHeight: 1000,
+    
     closeTooltip: 'x-button',  // x-button || onmouseout
     openTooltip: 'onclick'  // onclick, onmouseover, or any svg attr
 };
