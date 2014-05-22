9d62d907cb67272c23afd5312fbec34909ae9c2a
Tylar Murray <murray.tylar@gmail.com>
2014-05-22 00:09:31 -0400

    added missing comma

--- demo/tree_config.js
+++ demo/tree_config.js
@@ -3,7 +3,7 @@ treeConfig = {
     // svgSrc: '/demo/demo_resources/tree.svg',
     jsonSrc: 'http://7yl4r.github.io/techtreejs/demo/demo_tree.json',
     treeWidth: 650,
-    treeHeight: 600
+    treeHeight: 600,
     
     closeTooltip: 'x-button',  // x-button || onmouseout
     openTooltip: 'onclick'  // onclick, onmouseover, or any svg attr
